c96a7209defe5d2088b7cc6d2202903e6a750b14
sophie-owens <sophia_owens@student.davincischools.org>
2026-03-09 11:25:41 -0700

    Create ChallengeE.rb

---
+++ ChallengeE.rb
@@ -0,0 +1,56 @@
+# Challenge E
+use_bpm 180
+use_synth :chipbass
+
+# VARIABLES TO STORE SAMPLES
+its_me_mario = "C:/Users/sophia_owens/Downloads/mario_sounds/jump.wav"
+
+# IT'S A ME, MARIO! SAMPLE
+sample its_me_mario
+
+sleep 8
+
+# INTRO
+play :e4
+sleep 0.5
+play :e4
+sleep 1
+play :e4
+sleep 1
+play :c4
+sleep 0.5
+play :e4
+sleep 1
+play :g4
+sleep 2
+play :g3
+sleep 2
+
+
+# CUSTOM MARIO SAMPLES
+sample "C:/Users/sophia_owens/Downloads/mario_sounds/mario_beat.wav"
+
+sleep 2
+
+sample "C:/Users/sophia_owens/Downloads/mario_sounds/mario_intro.wav"
+
+sleep 2
+
+sample :bd_haus
+sleep 2
+
+sample "C:/Users/sophia_owens/Downloads/mario_sounds/coin.wav"
+
+
+sleep 2
+
+sample :vinyl_backspin
+sleep 2
+
+sample "C:/Users/sophia_owens/Downloads/Super Mario Bros. - Mushroom Sound Effect.mp3"
+
+sleep 2
+
+
+# MUSHROOM SAMPLE
+# https://www.youtube.com/watch?v=6G-k4zxou7Y
