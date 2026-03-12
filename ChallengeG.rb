6871723c4bb2fe86586f92562e70e03499656a4a
sophie-owens <sophia_owens@student.davincischools.org>
2026-03-12 12:32:03 -0700

    Update ChallengeG.rb

--- ChallengeG.rb
+++ ChallengeG.rb
@@ -16,3 +16,27 @@ live_loop :main_theme do
     i = 0
   end
 end
+
+
+# Seven Nation Army by The White Stripes - Challenge G
+
+use_bpm 120
+use_synth :fm
+use_synth_defaults sustain: 0.25
+
+stripesNotes = [:e3, :e3, :g3, :e3, :r, :d3, :c3, :b2]
+i = 0
+sleepTimes = [1.5, 0.5, 0.75, 0.25, 0.5, 0.5, 2, 2]
+s = 0
+
+live_loop :white_stripes do
+  8.times do
+    play (stripesNotes[i])
+    sleep (sleepTimes[s])
+    i = i + 1
+    s = s + 1
+    print i
+  end
+  i = 0
+  s = 0
+end
