57f487e996cbc5a98ef449fc491fb029415273ae
sophie-owens <sophia_owens@student.davincischools.org>
2026-03-12 11:31:02 -0700

    Update custom_song.rb

--- custom_song.rb
+++ custom_song.rb
@@ -1,5 +1,4 @@
 #It's All Right
-
 #Variables
 vocals = "C:/Users/sophia_owens/Desktop/say_it's_alr.mp3"
 chatter = "C:/Users/sophia_owens/Desktop/happy_chatter.mp3"
@@ -7,7 +6,7 @@ alr_piano = "C:/Users/sophia_owens/Desktop/it's_alr_piano.mp3"
 
 use_synth :piano
 #DEFINE a function that will play the notes
-define :intro_piano do
+define :intro_piano_parameter do |changing_note|
   sleep 2
   play :a4
   play :a5
@@ -16,16 +15,7 @@ define :intro_piano do
   play :b5
   sleep 0.5
   play :d5, sustain: 3
-  play :d6, sustain: 3
-  sleep 2
-  play :a4
-  play :a5
-  sleep 0.2
-  play :b4
-  play :b5
-  sleep 0.5
-  play :d4, sustain: 3
-  play :d5, sustain: 3
+  play changing_note, sustain: 3
 end
 
 live_loop :high_hat do
@@ -44,14 +34,28 @@ live_loop :high_hat do
 end
 
 live_loop :chatter do
-  sample chatter, amp: 3
-  sleep 1
+  sample chatter, start: 0, finish: 0.2, amp: 4
+  sleep 29.184172335600906/5
+  
+  sample chatter, start: 0.2, finish: 0.4, amp: 3
+  sleep 29.184172335600906/5
+  
+  sample chatter, start: 0.4, finish: 0.6, amp: 2
+  sleep 29.184172335600906/5
+  
+  sample chatter, start: 0.6, finish: 0.8, amp: 1
+  sleep 29.184172335600906/5
+  
+  sample chatter, start: 0.8, finish: 1, amp: 0.5
+  sleep 29.184172335600906/5
   stop
 end
 
 use_synth :piano
-intro_piano
-intro_piano
+2.times do
+  intro_piano_parameter :d6
+  intro_piano_parameter :d4
+end
 sleep 1
 
 live_loop :its_alr do
@@ -62,16 +66,19 @@ end
 
 sleep 1.5
 live_loop :its_alr_piano do
-  sample alr_piano
-  sleep 1
+  sample alr_piano, start: 0, finish: 0.2, amp: 4
+  sleep 16.893809523809523/5
+  
+  sample alr_piano, start: 0.2, finish: 0.4, amp: 3
+  sleep 16.893809523809523/5
+  
+  sample alr_piano, start: 0.4, finish: 0.6, amp: 2
+  sleep 16.893809523809523/5
+  
+  sample alr_piano, start: 0.6, finish: 0.8, amp: 1
+  sleep 16.893809523809523/5
+  
+  sample alr_piano, start: 0.8, finish: 1, amp: 0.5
+  sleep 16.893809523809523/5
   stop
-end
-
-sleep 16.5
-use_synth :piano
-with_bpm 100 do
-  with_fx :ping_pong do
-    play :g3, sustain: 6, amp: 2
-    sleep 1
-  end
 end
